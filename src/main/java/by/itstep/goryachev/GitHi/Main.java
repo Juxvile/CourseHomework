bc65e3f618cef6aa37b5a563e1b312142140fd10
Juxvile <andrugor1994@gmail.com>
2020-03-04 20:53:17 +0300

    Add .gitignore file.

--- src/main/java/by/itstep/goryachev/GitHi/Main.java
+++ src/main/java/by/itstep/goryachev/GitHi/Main.java
@@ -2,6 +2,8 @@ package by.itstep.goryachev.GitHi;
 
 public class Main {
     public static void main(String[] args) {
+
+        System.out.println("New Branch");
         System.out.println("New Branch");
     }
 }
