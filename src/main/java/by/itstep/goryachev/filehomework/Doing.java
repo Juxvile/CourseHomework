f874a8bf83eee4556b11062cb31a7ac058cff65e
Juxvile <andrugor1994@gmail.com>
2020-03-09 19:58:11 +0300

    Add .gitignore file.

--- src/main/java/by/itstep/goryachev/filehomework/Doing.java
+++ src/main/java/by/itstep/goryachev/filehomework/Doing.java
@@ -1,92 +1,95 @@
-//package by.itstep.goryachev.filehomework;
-//
-//import java.util.Scanner;
-//
-//public class Doing {
-//    User user = new User();
-//    Menu menu = new Menu();
-//    Scanner sc = new Scanner(System.in);
-//    int number = sc.nextInt();
-//
-//        switch (number) {
-//        case (1):
-//            System.out.println("Имя контакта:");
-//            String name = sc.nextLine();
-//            user.setName(name);
-//            System.out.println("Имя контакта записано успешно \n" +
-//                    "Что хотите записать дальше?");
-//            menu.content();
-//            break;
-//
-//
-//        case (2):
-//            System.out.println("Фамилия контакта:");
-//            String surname = sc.nextLine();
-//            user.setSurname(surname);
-//            System.out.println("Имя контакта записано успешно \n" +
-//                    "Что хотите записать дальше?");
-//            menu.content();
-//            break;
-//
-//
-//        case (3):
-//            System.out.println("Прозвище контакта:");
-//            String nickname = sc.nextLine();
-//            user.setNickname(nickname);
-//            System.out.println("Прозвище контакта записано успешно \n" +
-//                    "Что хотите записать дальше?");
-//            menu.content();
-//            break;
-//
-//        case (4):
-//            System.out.println("Домашний номер:");
-//            int homenumber = sc.nextInt();
-//            user.setHomenumber(homenumber);
-//            System.out.println("Домашний номер записан успешно \n" +
-//                    "Что хотите записать дальше?");
-//            menu.content();
-//            break;
-//
-//        case (5):
-//            System.out.println("Рабочий номер:");
-//            int jobnumber = sc.nextInt();
-//            user.setJobnumber(jobnumber);
-//            System.out.println("Рабочий номер записан успешно \n" +
-//                    "Что хотите записать дальше?");
-//            menu.content();
-//            break;
-//        case (6):
-//            System.out.println("Мобильный номер:");
-//            int mobilenumber = sc.nextInt();
-//            user.setMobilenumber(mobilenumber);
-//            System.out.println("Мобильный номер записан успешно \n" +
-//                    "Что хотите записать дальше?");
-//            menu.content();
-//            break;
-//        case (7):
-//            System.out.println("Факс:");
-//            int fax = sc.nextInt();
-//            user.setFax(fax);
-//            System.out.println("Факс записан успешно \n" +
-//                    "Что хотите записать дальше?");
-//            menu.content();
-//            break;
-//        case (8):
-//            System.out.println("Введите email:");
-//            String email = sc.nextLine();
-//            user.setEmail(email);
-//            System.out.println("Email записан успешно \n" +
-//                    "Что хотите записать дальше?");
-//            menu.content();
-//            break;
-//        case (9):
-//            System.out.println("Введите email:");
-//            int age = sc.nextInt();
-//            user.setAge(age);
-//            System.out.println("Год рождения записан успешно \n" +
-//                    "Что хотите записать дальше?");
-//            menu.content();
-//            break;
-//    }
-//
-//}
+package by.itstep.goryachev.filehomework;
+
+import java.util.ArrayList;
+import java.util.List;
+import java.util.Scanner;
+
+public class Doing {
+    public static void thismenu (int number) {
+        List<String> result = new ArrayList<>();
+        User user = new User();
+        Menu menu = new Menu();
+        Scanner sc = new Scanner(System.in);
+        int number1 = sc.nextInt();
+        switch (number) {
+            case (1):
+                System.out.println("Имя контакта:");
+                String name = sc.nextLine();
+                user.setName(name);
+                System.out.println("Имя контакта записано успешно \n" +
+                        "Что хотите записать дальше?");
+                menu.content();
+                break;
+
+
+            case (2):
+                System.out.println("Фамилия контакта:");
+                String surname = sc.nextLine();
+                user.setSurname(surname);
+                System.out.println("Имя контакта записано успешно \n" +
+                        "Что хотите записать дальше?");
+                menu.content();
+                break;
+
+
+            case (3):
+                System.out.println("Прозвище контакта:");
+                String nickname = sc.nextLine();
+                user.setNickname(nickname);
+                System.out.println("Прозвище контакта записано успешно \n" +
+                        "Что хотите записать дальше?");
+                menu.content();
+                break;
+
+            case (4):
+                System.out.println("Домашний номер:");
+                int homenumber = sc.nextInt();
+                user.setHomenumber(homenumber);
+                System.out.println("Домашний номер записан успешно \n" +
+                        "Что хотите записать дальше?");
+                menu.content();
+                break;
+
+            case (5):
+                System.out.println("Рабочий номер:");
+                int jobnumber = sc.nextInt();
+                user.setJobnumber(jobnumber);
+                System.out.println("Рабочий номер записан успешно \n" +
+                        "Что хотите записать дальше?");
+                menu.content();
+                break;
+            case (6):
+                System.out.println("Мобильный номер:");
+                int mobilenumber = sc.nextInt();
+                user.setMobilenumber(mobilenumber);
+                System.out.println("Мобильный номер записан успешно \n" +
+                        "Что хотите записать дальше?");
+                menu.content();
+                break;
+            case (7):
+                System.out.println("Факс:");
+                int fax = sc.nextInt();
+                user.setFax(fax);
+                System.out.println("Факс записан успешно \n" +
+                        "Что хотите записать дальше?");
+                menu.content();
+                break;
+            case (8):
+                System.out.println("Введите email:");
+                String email = sc.nextLine();
+                user.setEmail(email);
+                System.out.println("Email записан успешно \n" +
+                        "Что хотите записать дальше?");
+                menu.content();
+                break;
+            case (9):
+                System.out.println("Введите email:");
+                int age = sc.nextInt();
+                user.setAge(age);
+                System.out.println("Год рождения записан успешно \n" +
+                        "Что хотите записать дальше?");
+                menu.content();
+                break;
+        }
+    }
+}
